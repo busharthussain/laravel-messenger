cead0ae9bcf80ae9989960351e6bb712fdf2c0ab
bushart hussain <busharthussain@gmail.com>
2024-01-02 14:30:54 +0500

    update routes namespace

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('', 'App\Http\Controllers\MessengerController@index')->name(config('messenger.routes.prefix'));
-Route::post('messenger/store', 'App\Http\Controllers\MessengerController@store')->name('messenger.store');
-Route::get('messenger/user', 'App\Http\Controllers\MessengerController@getUser')->name('user.select');
-Route::get('messenger/user/conversation', 'App\Http\Controllers\MessengerController@getUserConversation')->name('user.conversation');
-Route::delete('message/delete', 'App\Http\Controllers\MessengerController@destroy')->name('message.delete');
-Route::post('messenger/mode/update', 'App\Http\Controllers\MessengerController@updateMessengerMode')->name('messenger.mode.update');
-Route::post('messenger/message/seen', 'App\Http\Controllers\MessengerController@messageSeen')->name('messenger.message.seen');
+Route::get('', 'MessengerController@index')->name(config('messenger.routes.prefix'));
+Route::post('messenger/store', 'MessengerController@store')->name('messenger.store');
+Route::get('messenger/user', 'MessengerController@getUser')->name('user.select');
+Route::get('messenger/user/conversation', 'MessengerController@getUserConversation')->name('user.conversation');
+Route::delete('message/delete', 'MessengerController@destroy')->name('message.delete');
+Route::post('messenger/mode/update', 'MessengerController@updateMessengerMode')->name('messenger.mode.update');
+Route::post('messenger/message/seen', 'MessengerController@messageSeen')->name('messenger.message.seen');
